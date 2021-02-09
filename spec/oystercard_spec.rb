8c7da57735ad113373b135ffe411f6b9ed155a84
jai-jk <jay.jk@hotmail.com>
2021-02-09 15:27:13 +0000

    Add tests and code to add money to card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-    it 'should default balance to 0' do
-        expect(subject.balance).to eq 0
+
+    it 'should default balance to DEFAULT_BALANCE' do
+        expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
+    end
+
+    it 'should add :top_up(amount) to :balance' do
+        expect { subject.top_up(5) }.to change { subject.balance }.by(5)
     end
 end
